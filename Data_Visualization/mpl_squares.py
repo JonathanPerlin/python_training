78a21ed864cd408b59388ad1bf339810c636c898
jonathanperlin <jonathan@perlin.com>
2020-06-26 18:44:03 -0400

    made a colorized random walk

--- Data_Visualization/mpl_squares.py
+++ Data_Visualization/mpl_squares.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 plt.plot(input_values, squares, linewidth=5)
@@ -10,6 +11,6 @@ plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-plt.tick_params(axis='both', labelsize=14)
+xplt.tick_params(axis='both', labelsize=14)
 
 plt.show()
