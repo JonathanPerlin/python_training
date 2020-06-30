d081eae355892edcd56c2e84aa531669f8bde165
jonathanperlin <jonathan@perlin.com>
2020-06-29 20:33:26 -0400

    rolling two dice and of different sizes

--- Data_Visualization/dice/different_dice.py
+++ Data_Visualization/dice/different_dice.py
@@ -2,9 +2,9 @@ import pygal
 
 from die import Die
 
-# Create two D6 dice.
+# Create two D6 and a D10.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
 results = []
@@ -22,10 +22,11 @@ for value in range(2, max_result+1):
 # Visualize the results.
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6 dice 10000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.title = "Results of rolling a D6 and D10 dice 50000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+                 '11', '12', '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6 + D6', frequencies)
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')
